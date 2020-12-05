0956126b75ea31ba30b642270fc04e370b703ae0
Joseph <jsphstan679@gmail.com>
2020-12-05 19:19:18 +0530

    Flask App v.1.2

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-@app.route("/app")
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template()
 
 if __name__ == "__main__":
     app.run()
